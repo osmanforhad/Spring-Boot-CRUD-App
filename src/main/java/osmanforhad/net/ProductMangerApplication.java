5628fb565bffec5ee0acba25dcbb16bb992eb861
Osman Forhad <osmanforhad93@gmail.com>
2020-02-26 16:07:56 +0600

    Spring Boot CRUD App

---
+++ src/main/java/osmanforhad/net/ProductMangerApplication.java
@@ -0,0 +1,13 @@
+package osmanforhad.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/osmanforhad/net/ProductMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package osmanforhad.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
