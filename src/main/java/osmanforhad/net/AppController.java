05eb653c2b98d91d73c6ceeb4294cf16362754a4
Osman Forhad <osmanforhad93@gmail.com>
2020-02-26 17:44:46 +0600

    write Controller class with Autowired Service class and Request Mapping

--- src/main/java/osmanforhad/net/AppController.java
+++ src/main/java/osmanforhad/net/AppController.java
@@ -1,5 +1,19 @@
 package osmanforhad.net;
 
-public class AppController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-}
+@Controller
+public class AppController {
+	
+	@Autowired
+private ProductService service;
+	
+	@RequestMapping("/")
+	public String viewHomePage() {
+		
+		return "index";
+	}//end of the viewHomePage method
+	
+}//end of the class
