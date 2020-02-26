e98946506cd27fb36fd07bcf99051e7370e12b8f
Osman Forhad <osmanforhad93@gmail.com>
2020-02-26 17:22:09 +0600

    write servic class and autowired interface with service class

--- src/main/java/osmanforhad/net/ProductService.java
+++ src/main/java/osmanforhad/net/ProductService.java
@@ -1,5 +1,32 @@
 package osmanforhad.net;
 
-public class ProductService {
+import java.util.List;
 
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+private ProductRepositroy repo;
+
+	public List<Product> listAll(){
+		
+		return repo.findAll();
+		
+	}//end of the listAll method
+	
+	public void save(Product product) {
+		repo.save(product);
+	}//end of the save method
+	
+	public Product get(Long id) {
+		return repo.findById(id).get();
+	}//end of the Product get method
+	
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}//end of the delete method
+
+}//end of the class
